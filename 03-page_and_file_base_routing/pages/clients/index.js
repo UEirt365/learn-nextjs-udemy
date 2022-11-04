3c20dbc47eb4f593180beeae732d0783a3fbde11
UEirt365 <nguyen.dinh.trieu@gmail.com>
2022-11-04 15:19:41 +0700

    03 - (63) Navigation to dynamic routes

--- 03-page_and_file_base_routing/pages/clients/index.js
+++ 03-page_and_file_base_routing/pages/clients/index.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
 export default function ClientPage() {
+  const clients = [
+    { id: "1", name: "Client 1" },
+    { id: "2", name: "Client 2" },
+    { id: "3", name: "Client 3" },
+  ];
   return (
     <div>
       <h1>The ClientPage</h1>
+      <ul>
+        {clients.map((client) => {
+          return (
+            <li key={client.id}>
+              <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
