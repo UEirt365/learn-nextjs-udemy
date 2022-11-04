6526e4890d8ffe8f741bd91b1e0f0a1048036e62
UEirt365 <nguyen.dinh.trieu@gmail.com>
2022-11-04 14:30:44 +0700

    03 - (59) Extracting Dynamic Path Segment Data (Dynamic Routes)

--- 03-page_and_file_base_routing/pages/portfolio/[projectId].js
+++ 03-page_and_file_base_routing/pages/portfolio/[projectId].js
@@ -1,7 +1,11 @@
+import { useRouter } from "next/router";
+
 export default function PortfolioProjectPage() {
+  const router = useRouter();
+
   return (
     <div>
-      <h1>The PortfolioProjectPage</h1>
+      <h1>The PortfolioProjectPage [{router.query.projectId}]</h1>
     </div>
   );
 }
