0d219419dd03fdd44ac61a67e0d260592f00e18b
UEirt365 <nguyendinhtrieu365@gmail.com>
2022-11-04 21:59:02 +0700

    03 - (65) Navigating Programatically

--- 03-page_and_file_base_routing/pages/clients/[clientId]/index.js
+++ 03-page_and_file_base_routing/pages/clients/[clientId]/index.js
@@ -2,9 +2,19 @@ import { useRouter } from "next/router";
 
 export default function ClientDetailPage() {
   const router = useRouter();
+  const viewProductHandler = () => {
+    router.push({
+      pathname: "/clients/[clientId]/[productId]",
+      query: {
+        clientId: router.query.clientId,
+        productId: `productId_client_${router.query.clientId}`,
+      },
+    });
+  };
   return (
     <div>
       <h1>The ClientDetailPage of [{router.query.clientId}]</h1>
+      <button onClick={viewProductHandler}>View Product</button>
     </div>
   );
 }
