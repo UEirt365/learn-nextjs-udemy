126b2659dcaf0f2a6567d3bfb5111e4f240f2473
UEirt365 <nguyendinhtrieu365@gmail.com>
2022-11-07 16:25:39 +0700

    04 - (75) Styling Component In Next.js Projects

--- 04-working_with_base_routing/components/events/event-list.js
+++ 04-working_with_base_routing/components/events/event-list.js
@@ -1,8 +1,9 @@
 import EventItem from "./event-item";
+import classes from "./event-list.module.css";
 
 export default function EventList(props) {
   return (
-    <ul>
+    <ul className={classes.list}>
       {props.events.map((event) => {
         return <EventItem key={event.id} {...event}></EventItem>;
       })}
--- 04-working_with_base_routing/pages/index.js
+++ 04-working_with_base_routing/pages/index.js
@@ -1,11 +1,10 @@
 import EventList from "../components/events/event-list";
-import { getFeaturedEvents } from "../dummy-data";
+import { getAllEvents, getFeaturedEvents } from "../dummy-data";
 
 export default function HomePage() {
-  const events = getFeaturedEvents();
+  const events = getAllEvents();
   return (
     <div>
-      <h1>HomePage</h1>
       <EventList events={events} />
     </div>
   );
