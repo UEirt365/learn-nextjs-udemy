dfc9240e731a310341ad3cfdd92c2db4bb27fed6
UEirt365 <nguyendinhtrieu365@gmail.com>
2022-11-09 15:15:29 +0700

    04 - (81) Adding a Filter Form for Filtering Events

--- 04-working_with_base_routing/components/ui/button.js
+++ 04-working_with_base_routing/components/ui/button.js
@@ -2,9 +2,13 @@ import Link from "next/link";
 import classes from "./button.module.css";
 
 export default function Button(props) {
-  return (
+  return props.link ? (
     <Link href={props.link}>
       <a className={classes.btn}>{props.children}</a>
     </Link>
+  ) : (
+    <button className={classes.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
   );
 }
--- 04-working_with_base_routing/pages/events/index.js
+++ 04-working_with_base_routing/pages/events/index.js
@@ -1,10 +1,12 @@
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 import { getAllEvents } from "../../dummy-data";
 
 export default function AllEventList() {
   const events = getAllEvents();
   return (
     <div>
+      <EventsSearch />
       <EventList events={events} />
     </div>
   );
