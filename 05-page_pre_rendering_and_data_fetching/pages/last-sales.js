20e23b2a49b1038fda65667f7997f2ac68580f95
UEirt365 <nguyendinhtrieu365@gmail.com>
2022-11-17 11:01:35 +0700

    05 - (113) Using the useSWR NextJS Hook

--- 05-page_pre_rendering_and_data_fetching/pages/last-sales.js
+++ 05-page_pre_rendering_and_data_fetching/pages/last-sales.js
@@ -1,34 +1,33 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 export default function LastSales() {
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+
+  const { data, error } = useSWR(
+    "https://fir-app-demo-55852-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json",
+    (url) => fetch(url).then((res) => res.json())
+  );
 
   useEffect(() => {
-    setIsLoading(true);
-    fetch(
-      "https://fir-app-demo-55852-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        let transformedSales = [];
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setIsLoading(false);
-        setSales(transformedSales);
-      });
-  }, []);
+    if (data) {
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-  if (!sales) {
+  if (error) {
     return <p>No data found!</p>;
+  }
+  if (!data || !sales) {
+    return <p>Loading...</p>;
   }
 
   return (
