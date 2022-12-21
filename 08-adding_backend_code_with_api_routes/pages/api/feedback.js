8d0b52c5d96100ae07cf13b63c8b46aab600be93
UEirt365 <nguyendinhtrieu365@gmail.com>
2022-12-21 15:03:51 +0700

    08 - (145) Sending the incoming request & executing server-side code

--- 08-adding_backend_code_with_api_routes/pages/api/feedback.js
+++ 08-adding_backend_code_with_api_routes/pages/api/feedback.js
@@ -1,3 +1,16 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "success" });
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const feedback = req.body.feedback;
+
+    const newFeedback = {
+      id: new Date().getTime(),
+      email,
+      feedback,
+    };
+    console.log(newFeedback);
+    res.status(201).json({ message: "success", feedback: newFeedback });
+  } else {
+    res.status(200).json({ message: "success" });
+  }
 }
