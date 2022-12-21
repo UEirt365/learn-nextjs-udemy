4e56379fd84337324d40e0f06a57fd4e4c55830b
UEirt365 <nguyendinhtrieu365@gmail.com>
2022-12-21 14:47:37 +0700

    08 - (144) Preparing the Frontend Form

--- 08-adding_backend_code_with_api_routes/pages/index.js
+++ 08-adding_backend_code_with_api_routes/pages/index.js
@@ -1,7 +1,32 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailRef = useRef();
+  const feedbackRef = useRef();
+
+  const submitFormHandler = (e) => {
+    e.preventDefault();
+
+    const email = emailRef.current.value;
+    const feedback = feedbackRef.current.value;
+
+    console.log({ email, feedback });
+  };
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" ref={emailRef}></input>
+        </div>
+        <div>
+          <label htmlFor="feedback">Feedback</label>
+          <textarea id="feedback" name="feedback" ref={feedbackRef}></textarea>
+        </div>
+        <button type="submit">Submit Feedback</button>
+      </form>
     </div>
   );
 }
