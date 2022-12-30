90bc89a81d4cb6e63faa63d18c9587ec074f787a
UEirt365 <nguyendinhtrieu365@gmail.com>
2022-12-30 14:38:02 +0700

    10- (171) Adding Context State

--- 10-working_with_app_wide_state/store/notification-context.js
+++ 10-working_with_app_wide_state/store/notification-context.js
@@ -1,14 +1,30 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export default NotificationContext = createContext({
   notification: null,
-  showNotifications: function () {},
-  hideNotifications: function () {},
+  showNotification: function (notificationData) {},
+  hideNotification: function () {},
 });
 
 export const NotificationContextProvider = (props) => {
+  const [activeNotification, setActiveNotification] = useState();
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
   return (
-    <NotificationContext.Provider>
+    <NotificationContext.Provider value={context}>
       {props.children}
     </NotificationContext.Provider>
   );
